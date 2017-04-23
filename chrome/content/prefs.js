13ac5c65f10084895e44aeb90de112edb80474fb
Trever54 <Trevermock@gmail.com>
2017-04-23 11:20:26 -0600

    cleaned up prefs.js according to eslint

--- chrome/content/prefs.js
+++ chrome/content/prefs.js
@@ -1,3 +1,5 @@
+/* global pref */
+
 // used to test the migration code:
 // pref ("extensions.themeswitcher.defshift", false);
 
@@ -64,4 +66,5 @@ pref ("extensions.personaswitcher.current", 0);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 
-pref ("extensions.drsjb80@gmail.com.description", "chrome://personaswitcher/locale/overlay.properties");
+pref ("extensions.drsjb80@gmail.com.description", 
+"chrome://personaswitcher/locale/overlay.properties");
