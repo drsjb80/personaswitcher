58a63354b1fadb1b77f7590dda894f374bbd8703
Trever54 <Trevermock@gmail.com>
2017-04-24 14:56:05 -0600

    updated popup.js to more eslint standards

--- webextension/browser_action/popup.js
+++ webextension/browser_action/popup.js
@@ -12,8 +12,8 @@ function appendMenu(page)
     getPreferences.then((results) => 
     {
         backgroundPage.logger.log(
-			"Creating a new menu:" + !results[0].staticMenus);
-        if(false === results[0].staticMenus) 
+            "Creating a new menu:" + !results[0].staticMenus);
+        if(results[0].staticMenus === false) 
         {
             var gettingMenuData = backgroundPage.getMenuData();
             gettingMenuData
@@ -36,11 +36,11 @@ function appendMenu(page)
 function removeMenu() 
 {
     document.body.removeChild(backgroundPage.browserActionMenu);
-    //The ownerDocument is set as the last DOM that the element was assigned to.
-    //If the menu's ownerDocument remains this window, it will be marked as a 
-    //deadObject when this window finishes unloading. Since we don't want to 
-    //have to rebuild it, we need to assign it to a window that isn't going to
-    //be closed.
+    // The ownerDocument is set as the last DOM that the element was assigned to.
+    // If the menu's ownerDocument remains this window, it will be marked as a 
+    // deadObject when this window finishes unloading. Since we don't want to 
+    // have to rebuild it, we need to assign it to a window that isn't going to
+    // be closed.
     backgroundPage.document.body.appendChild(backgroundPage.browserActionMenu);
 }
 
