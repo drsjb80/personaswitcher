d8b0f56eb3362e60a59aad6e4bba6f91e8bf3324
Tj Virbick <tjvirb@gmail.com>
2020-02-19 13:47:09 -0700

    Remove maunal refresh option from menu

--- background_scripts/menus.js
+++ background_scripts/menus.js
@@ -192,12 +192,6 @@ function buildContextMenu()
           title: "Persona Switcher Options",
           contexts: ["browser_action"]
     });
-    browser.menus.create(
-    {
-          id: "ReloadThemes",
-          title: "Refresh PSwitcher Themes",
-          contexts: ["browser_action"]
-    });
 }
 
 
