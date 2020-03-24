40dedae761858b3e6e43d135e73d80f51ff27dd2
drsjb80 <drsjb80@gmail.com>
2020-03-24 14:53:34 -0600

    initial

---
+++ tests/themes.test.js
@@ -0,0 +1,13 @@
+const {themes, getDefaultThemeIndex} = require('../background_scripts/themes');
+
+// const { getDefaultThemeIndex, themes } = require('../background_scripts/themes');
+
+test('no default themes', () => {
+    expect(getDefaultThemeIndex()).toBe(0);
+});
+
+test('one default theme', () => {
+    themes.defaultTheme = { "id": "bar" };
+    themes.defaultThemes = [{"id": "foo"}, {"id": "bar"}];
+    expect(getDefaultThemeIndex()).toBe(2);
+});
